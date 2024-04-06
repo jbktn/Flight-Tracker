1f2eb6973c444c010bd1c04042a0962ff4bd4dc6
Jakub Pomorski <delveller1@gmail.com>
2024-04-06 19:03:18 +0200

    initial changes

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Navbar: React.FC = () => {
     return (
-        <nav className='bg-gradient-to-b from-cyan-400 to-white'>
-
+        <nav className='h-16 bg-gradient-to-b from-blue-500 to-blue-300'>
+            <div className='text-3xl text-blue-50 font-bold'>Flight Tracker</div>
         </nav>
     );
 };
