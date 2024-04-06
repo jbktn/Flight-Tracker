0d28a325f8635cac9a621138bf0d30b8f82589e4
Jakub Pomorski <delveller1@gmail.com>
2024-04-06 18:28:26 +0200

    initial commit

---
+++ src/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar: React.FC = () => {
+    return (
+        <nav className='bg-gradient-to-b from-cyan-400 to-white'>
+
+        </nav>
+    );
+};
+
+export default Navbar;
